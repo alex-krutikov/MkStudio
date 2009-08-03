ac5bbb810b46e25fe4839b4f1c11b20650a4bbcb
Alex <alex-krutikov@yandex.ru>
2009-08-03 17:26:06 +0400

    Mkserver: в эмулятор добавлена поддержка регистрового модбаса.
    Signed-off-by: Alex <alex-krutikov@yandex.ru>

--- src/mkserver/mainwindow.cpp
+++ src/mkserver/mainwindow.cpp
@@ -27,49 +27,255 @@ public:
   int query( const QByteArray &req, QByteArray &ans, int *errorcode=0)
   {
     Q_UNUSED( errorcode );
-    
+
     static unsigned char base[100000];
+
+    if( req.size() < 4 ) return 0;
+    if( CRC::CRC16( req ) )
+    { Console::Print( Console::ModbusError,
+            "Request packet: CRC ERROR. [ " + QByteArray2QString( req ) + " ]\n" );
+      return 0;
+    }
 
     ans.resize( 300 );
 
-    int addr = ( (unsigned char)req[3] << 8 ) | (unsigned char)req[4];
-    int len  = (unsigned char)req[5];
     int ans_len = 0;
     int i;
 
+
     ans[0] = req[0];
     ans[1] = req[1];
-    ans[2] = req[2];
-    ans[3] = req[3];
-    ans[4] = req[4];
 
-    switch( req[2] & 0x0F )
+    switch( req[1] ) // modbus function
     {
-      case( 0x00 ): // read
-        ans[3] = len;
-        for(i=0; i<len; i++) ans[4+i] = base[addr+i];
-        ans_len = len + 6;
+      // classic modbus: coils and discrete inputs read
+      case( 0x01 ):
+      case( 0x02 ):
+      {
+        if( req.size() != 8 ) return 0;
+
+        int first_reg = (unsigned char)req[2]<<8 | (unsigned char)req[3];
+        int quantity  = (unsigned char)req[4]<<8 | (unsigned char)req[5];
+
+        if( ( quantity < 1 ) || ( quantity > 2000 ) )
+        { ans[1] = req[1] | 0x80;
+          ans[2] = 3; // modbus exception code: illegal data value
+          ans_len = 5;
+          break;
+        }
+
+        int len  = (quantity-1)/8+1;
+        ans[2] = len;
+
+        for(i=0; i<len; i++)
+        { ans[3+i]=0;
+        }
+
+        for(i=0; i<quantity; i++)
+        { if( base[(first_reg+i)>>3] & (1<<((first_reg+i)&0x07)) )
+          { ans[3+(i>>3)] = ans[3+(i>>3)] | (1<<(i&0x07));
+          }
+        }
+        ans_len = len + 5;
         break;
-      case( 0x01 ): // set
-        ans[5] = len;
-        for(i=0; i<len; i++) base[addr+i] = req[6+i];
-        ans_len = len + 8;
+      }
+
+      // classic modbus: holding and input registers read
+      case( 0x03 ):
+      case( 0x04 ):
+      {
+        if( req.size() != 8 ) return 0;
+
+        int first_reg = (unsigned char)req[2]<<8 | (unsigned char)req[3];
+        int quantity  = (unsigned char)req[4]<<8 | (unsigned char)req[5];
+
+        if( ( quantity < 1 ) || ( quantity > 125 ) )
+        { ans[1] = req[1] | 0x80;
+          ans[2] = 3; // modbus exception code: illegal data value
+          ans_len = 5;
+          break;
+        }
+
+        int len  = 2*quantity;
+        if( len > 250 ) break;
+        ans[2] = len;
+        for(i=0; i<len; i++) ans[3+i] = base[first_reg+(i^1)];
+        ans_len = len + 5;
         break;
-      case( 0x03 ): // and
-        ans[5] = len;
-        for(i=0; i<len; i++) base[addr+i] &= req[6+i];
-        ans_len = len + 8;
+      }
+
+      // classic modbus: write single coil
+      case( 0x05 ):
+      {
+        if( req.size() != 8 ) return 0;
+
+        int first_reg = (unsigned char)req[2]<<8 | (unsigned char)req[3];
+        int value     = (unsigned char)req[4]<<8 | (unsigned char)req[5];
+        ans = req;
+        switch( value )
+        { case( 0x0000 ):
+            base[(first_reg)>>3] &= ~(1<<((first_reg)&0x07));
+            ans_len = 8;
+            break;
+          case( 0xFF00 ):
+            base[(first_reg)>>3] |= (1<<((first_reg)&0x07));
+            ans_len = 8;
+            break;
+          default:
+           ans[1] = req[1] | 0x80;
+           ans[2] = 3; // modbus exception code: illegal data value
+           ans_len = 5;
+           break;
+        }
         break;
-      case( 0x05 ): // or
-        ans[5] = len;
-        for(i=0; i<len; i++) base[addr+i] |= req[6+i];
-        ans_len = len + 8;
+      }
+
+      // classic modbus: write single register
+      case( 0x06 ):
+      {
+        if( req.size() != 8 ) return 0;
+
+        int first_reg = (unsigned char)req[2]<<8 | (unsigned char)req[3];
+        ans = req;
+        base[first_reg  ] = req[5];
+        base[first_reg+1] = req[4];
+        ans_len = 8;
         break;
-      case( 0x07 ): // xor
-        ans[5] = len;
-        for(i=0; i<len; i++) base[addr+i] ^= req[6+i];
-        ans_len = len + 8;
-      break;
+      }
+
+      // classic modbus: write multiple coil
+      case( 0x0F ):
+      {
+        if( req.size() < 9 ) return 0;
+
+        int first_reg = (unsigned char)req[2]<<8 | (unsigned char)req[3];
+        int quantity  = (unsigned char)req[4]<<8 | (unsigned char)req[5];
+
+        if( ( quantity < 1 ) || ( quantity > 2000 ) )
+        { ans[1] = req[1] | 0x80;
+          ans[2] = 3; // modbus exception code: illegal data value
+          ans_len = 5;
+          break;
+        }
+
+        int len  = (quantity-1)/8+1;
+
+        if( (unsigned int)len != (unsigned int)req[6] )
+        { ans[1] = req[1] | 0x80;
+          ans[2] = 3; // modbus exception code: illegal data value
+          ans_len = 5;
+          break;
+        }
+
+        if( req.size() != ( 9 + len ) ) return 0;
+
+        ans[2] = req[2];
+        ans[3] = req[3];
+        ans[4] = req[4];
+        ans[5] = req[5];
+
+        for( i=0; i<quantity; i++ )
+        {
+          if( req[7+(i>>3)] & (1<<(i&0x07)) )
+          { base[(first_reg+i)>>3] |= (1<<((first_reg+i)&0x07));
+          } else
+          { base[(first_reg+i)>>3] &= ~(1<<((first_reg+i)&0x07));
+          }
+        }
+        ans_len = 8;
+        break;
+      }
+
+      // classic modbus: write multiple registers
+      case( 0x10 ):
+      {
+        if( req.size() < 9 ) return 0;
+
+        int first_reg = (unsigned char)req[2]<<8 | (unsigned char)req[3];
+        int quantity  = (unsigned char)req[4]<<8 | (unsigned char)req[5];
+
+        if( ( quantity < 1 ) || ( quantity > 125 ) )
+        { ans[1] = req[1] | 0x80;
+          ans[2] = 3; // modbus exception code: illegal data value
+          ans_len = 5;
+          break;
+        }
+
+        int len  = 2*quantity;
+
+        if( (unsigned int)len != (unsigned int)req[6] )
+        { ans[1] = req[1] | 0x80;
+          ans[2] = 3; // modbus exception code: illegal data value
+          ans_len = 5;
+          break;
+        }
+
+        if( req.size() != ( 9 + len ) ) return 0;
+
+        ans[2] = req[2];
+        ans[3] = req[3];
+        ans[4] = req[4];
+        ans[5] = req[5];
+
+        for( i=0; i<len; i++ )
+        {
+          base[first_reg+i] = req[ 7+(i^1) ];
+        }
+        ans_len = 8;
+        break;
+      }
+
+
+      // mikkon
+      case( 0x41 ):
+      case( 0x43 ):
+      {
+        int addr = ( (unsigned char)req[3] << 8 ) | (unsigned char)req[4];
+        int len  = (unsigned char)req[5];
+        ans[2] = req[2];
+        ans[3] = req[3];
+        ans[4] = req[4];
+        switch( req[2] & 0x0F )
+        {
+          case( 0x00 ): // read
+            ans[3] = len;
+            for(i=0; i<len; i++) ans[4+i] = base[addr+i];
+            ans_len = len + 6;
+            break;
+          case( 0x01 ): // set
+            ans[5] = len;
+            for(i=0; i<len; i++) base[addr+i] = req[6+i];
+            ans_len = len + 8;
+            break;
+          case( 0x03 ): // and
+            ans[5] = len;
+            for(i=0; i<len; i++) base[addr+i] &= req[6+i];
+            ans_len = len + 8;
+            break;
+          case( 0x05 ): // or
+            ans[5] = len;
+            for(i=0; i<len; i++) base[addr+i] |= req[6+i];
+            ans_len = len + 8;
+            break;
+          case( 0x07 ): // xor
+            ans[5] = len;
+            for(i=0; i<len; i++) base[addr+i] ^= req[6+i];
+            ans_len = len + 8;
+            break;
+          default:
+           ans[1] = req[1] | 0x80;
+           ans[2] = 4; // modbus exception code: slave device failure
+           ans_len = 5;
+           break;
+        }
+        break;
+      }
+    }
+
+    if( ans_len == 0 ) // error: unknown function
+    { ans[1] = req[0] | 0x80;
+      ans[2] = 1; // modbus exception code: illegal function
+      ans_len = 5;
     }
 
     if( ans_len > 2)
